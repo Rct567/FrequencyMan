e864a0ec9cbe819d0e277a7fc311c6baf413acca
Rick <rct999@gmail.com>
2023-12-03 03:45:29 +0100

    small improvements in test_target_list_reorder

--- tests/test_target_list_reorder.py
+++ tests/test_target_list_reorder.py
@@ -16,13 +16,9 @@ def store_card_id_list(filename: str, int_list: list[CardId]) -> None:
         file.write('\n'.join(map(str, int_list)))
 
 def read_card_id_list(filename: str) -> list[CardId]:
-    try:
-        with open(filename, 'r') as file:
-            int_list = [CardId(int(line.strip())) for line in file]
-        return int_list
-    except FileNotFoundError:
-        print(f"Error: File '{filename}' not found.")
-        return []
+    with open(filename, 'r') as file:
+        int_list = [CardId(int(line.strip())) for line in file]
+    return int_list
 
 
 class TestTargetListReorder:
@@ -30,25 +26,36 @@ class TestTargetListReorder:
     TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
     TEST_COLLECTIONS_DIR = os.path.join(TEST_DATA_DIR, 'collections')
 
-    def get_test_collection(self, test_collection_name: str):
+    def get_test_collection(self, test_collection_name: str, collection_test_seq_num:int):
 
         collection_dir = os.path.join(self.TEST_COLLECTIONS_DIR, test_collection_name)
 
         word_frequency_lists = WordFrequencyLists(os.path.join(collection_dir, 'frequency_lists'))
 
-        # temp collection
+        # create temporary collection
         collection_file = os.path.join(collection_dir, test_collection_name+".anki2")
-        temp_collection_file = os.path.join(collection_dir, test_collection_name+"_temp.anki2")
+        temp_collection_file = os.path.join(collection_dir, test_collection_name+"_"+str(collection_test_seq_num)+"_temp.anki2")
         if os.path.exists(temp_collection_file):
             os.remove(temp_collection_file)
         shutil.copy(collection_file, temp_collection_file)
 
+        # helper function used to lock-in and assert order
+        def assert_locked_order(sorted_cards_ids: list[CardId]):
+            order_file = os.path.join(collection_dir, 'expected_order.txt')
+
+            if os.path.exists(order_file):
+                expected_order = read_card_id_list(order_file)
+                assert sorted_cards_ids == expected_order
+            else:
+                store_card_id_list(order_file, sorted_cards_ids)
+                print("WARNING: Order file for '{}' didn't exist yet!".format(test_collection_name))
+
         # return result
-        return collection_dir, Collection(temp_collection_file), word_frequency_lists
+        return Collection(temp_collection_file), word_frequency_lists, assert_locked_order
 
     def test_reorder_cards_big_collection_es(self):
 
-        collection_dir, col, word_frequency_lists = self.get_test_collection('big_collection_es')
+        col, word_frequency_lists, assert_locked_order = self.get_test_collection('big_collection_es', collection_test_seq_num=0)
 
         target_list = TargetList(word_frequency_lists)
 
@@ -65,11 +72,11 @@ class TestTargetListReorder:
             }
         ])
 
+        assert len(target_list) == 1
         pre_sort_cards = target_list[0].get_cards(col)
-
         event_logger = EventLogger()
 
-        # sort
+        # reorder cards
         result = target_list.reorder_cards(col, event_logger)
 
         assert isinstance(result, TargetListReorderResult)
@@ -78,8 +85,6 @@ class TestTargetListReorder:
         # check result
 
         reorder_result = result.reorder_result_list[0]
-        #print(str(event_logger))
-
         assert reorder_result.success
         assert reorder_result.repositioning_required
         assert reorder_result.error is None
@@ -91,12 +96,6 @@ class TestTargetListReorder:
         assert "Reposition cards from targets" in str(event_logger)
 
         # check order
+        assert_locked_order(reorder_result.sorted_cards_ids)
 
-        order_file = os.path.join(collection_dir, 'expected_order.txt')
 
-        if os.path.exists(order_file):
-            expected_order = read_card_id_list(order_file)
-            assert reorder_result.sorted_cards_ids == expected_order
-        else:
-            store_card_id_list(order_file, reorder_result.sorted_cards_ids)
-            print("WARNING: Order file for '{}' didn't exist yet!".format("big_collection_es"))

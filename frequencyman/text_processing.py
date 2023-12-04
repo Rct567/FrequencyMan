e15898c4821ab0184db022c82e3984a7dbe1f5e7
Rick <rct999@gmail.com>
2023-12-04 04:50:19 +0100

    some optimizations in acceptable_word

--- frequencyman/text_processing.py
+++ frequencyman/text_processing.py
@@ -7,6 +7,12 @@ WordToken = NewType('WordToken', str)
 
 class TextProcessing:
 
+    CHINESE_JAPANESE_PATTERN = re.compile(r'[\u4e00-\u9fff]{1,}', re.UNICODE)
+    KOREAN_PATTERN = re.compile(r'[\uac00-\ud7a3]{1,}', re.UNICODE)
+    ARABIC_PATTERN = re.compile(r'[\u0600-\u06FF]{1,}', re.UNICODE)
+    ETHIOPIC_PATTERN = re.compile(r'[\u1200-\u137F]{1,}', re.UNICODE)
+    DEFAULT_PATTERN = re.compile(r'[\w]{2,}', re.UNICODE)
+
     @staticmethod
     def acceptable_word(word: str, lang_id: Optional[str] = None) -> bool:
 
@@ -14,20 +20,26 @@ class TextProcessing:
             raise ValueError(f"Invalid lang_id '{lang_id}'!")
 
         min_length = 1
-        if lang_id in ['zh', 'ja']:
-            word_pattern = re.compile(r'[\u4e00-\u9fff]{1,}', re.UNICODE)  # Chinese and Japanese characters
-        elif lang_id in ['ko']:
-            word_pattern = re.compile(r'[\uac00-\ud7a3]{1,}', re.UNICODE)  # Korean characters
-        elif lang_id in ['ar']:
-            word_pattern = re.compile(r'[\u0600-\u06FF]{1,}', re.UNICODE)  # Arabic characters
-        elif lang_id in ['am', 'ti', 'om', 'so', 'ha']:
-            word_pattern = re.compile(r'[\u1200-\u137F]{1,}', re.UNICODE)  # Ethiopic characters
+        if lang_id == 'zh':
+            word_pattern = TextProcessing.CHINESE_JAPANESE_PATTERN
+        elif lang_id == 'ja':
+            word_pattern = TextProcessing.CHINESE_JAPANESE_PATTERN
+        elif lang_id == 'ko':
+            word_pattern = TextProcessing.KOREAN_PATTERN
+        elif lang_id == 'ar':
+            word_pattern = TextProcessing.ARABIC_PATTERN
+        elif lang_id in {'am', 'ti', 'om', 'so', 'ha'}:
+            word_pattern = TextProcessing.ETHIOPIC_PATTERN
         else:
-            word_pattern = re.compile(r'[\w]{2,}', re.UNICODE)
+            word_pattern = TextProcessing.DEFAULT_PATTERN
             min_length = 2
 
         stripped_word = word.strip("!@#$%^&*()_-=+{}:\"<>?,./;' ")
-        return len(stripped_word) >= min_length and len(stripped_word) < 300 and bool(re.search(word_pattern, stripped_word)) and not stripped_word.isdigit()
+        stripped_word_len = len(stripped_word)
+        if stripped_word_len < min_length or stripped_word_len > 300:
+            return False
+
+        return bool(re.search(word_pattern, stripped_word)) and not stripped_word.isdigit()
 
     @staticmethod
     def get_plain_text(val: str) -> str:
